ec68c7c28c80a7f695040e9f0dba8c0d86886b53
KenlaSystems <kenlasystems@KenlaMac002.local>
2016-02-03 18:42:53 +0530

    Initial Commit

---
+++ CapsConverter/CapsConverter.h
@@ -0,0 +1,19 @@
+//
+//  CapsConverter.h
+//  CapsConverter
+//
+//  Created by KenlaSystems on 03/02/16.
+//  Copyright (c) 2016 KenlaSystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CapsConverter.
+FOUNDATION_EXPORT double CapsConverterVersionNumber;
+
+//! Project version string for CapsConverter.
+FOUNDATION_EXPORT const unsigned char CapsConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CapsConverter/PublicHeader.h>
+
+
